a20114aa365e22bb4302b63783606e6a29b436e1
Wesley <mr.chung402@gmail.com>
2018-11-10 17:19:37 -0500

    add new bash

--- bash/start_kafka.sh
+++ bash/start_kafka.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cd ../tools/zookeeper-3.4.12
-bin/zkServer.sh start
-cd ../kafka_2.11-2.0.0
+cd ../tools/kafka_2.11-2.0.0
 bin/kafka-server-start.sh config/server.properties
---
+++ bash/start_zookeeper.sh
@@ -0,0 +1,2 @@
+cd ../tools/zookeeper-3.4.12
+bin/zkServer.sh start
