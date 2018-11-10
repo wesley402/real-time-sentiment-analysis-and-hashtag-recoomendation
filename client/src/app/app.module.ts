178721c73ead74eee0c72cca29361807f3e06527
Wesley <mr.chung402@gmail.com>
2018-11-10 17:26:09 -0500

    remodule app

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { HttpModule } from "@angular/http";
+import { TrendingByTimeComponent } from './components/trending-by-time/trending-by-time.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrendingByTimeComponent
   ],
   imports: [
     BrowserModule,
